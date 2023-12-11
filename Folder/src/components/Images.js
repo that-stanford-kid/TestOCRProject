1556934cc511f16b1cee9d4665e0193c4ff7835b
Patrick O'Neil <58711617+that-stanford-kid@users.noreply.github.com>
2023-12-11 07:19:56 -0600

    Images.js

---
+++ Folder/src/components/Images.js
@@ -0,0 +1,9 @@
+// src/components/Image.js
+import React from ;react';
+import logo from '.../assets/bricks.png'; // example colored squared staggered if wanted.
+
+const Image = () => {
+  return <img src={logo} alt="logo" />;
+};
+
+export default Image;
