15414a44c9cccbeddf7807ce3d9f3988ec5cff70
Patrick O'Neil <58711617+that-stanford-kid@users.noreply.github.com>
2023-12-11 07:58:06 -0600

    logger.js

--- Folder/src/components/backend/logger.js
+++ Folder/src/components/backend/logger.js
@@ -10,7 +10,7 @@ const logger = createLogger({
   ],
 });
 
-if (process.env.NODE_ENV !== 'production) {
+if (process.env.NODE_ENV !== 'production') {
     logger.add(new transports.Console({ format: format.simple() }));
 }
 
